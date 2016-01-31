54b78e07f7eefa0909b57a36bc043d4130e9701e
afujisak <a.korira@gmail.com>
2016-01-31 14:30:02 +0900

    devicemotion

---
+++ js/deviceMotion.js
@@ -0,0 +1,16 @@
+$(function(){
+    //加速度のイベント
+    window.addEventLister('devicemotion',shakeDevice);
+
+
+    function shakeDevice(evt){
+        var x = evt.acceleration.x;
+        var y = evt.acceleration.y;
+        var z = evt.acceleration.z;
+
+        $('#device li').eq(0).text('加速度x: ' + x);
+        $('#device li').eq(1).text('加速度x: ' + y);
+        $('#device li').eq(2).text('加速度x: ' + z);        
+    }
+
+});
