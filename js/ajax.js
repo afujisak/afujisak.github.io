f48dfa8a587645879b8545f8e7bb8a2fee704eb4
afujisak <a.korira@gmail.com>
2016-01-31 16:21:11 +0900

    ajax

---
+++ js/ajax.js
@@ -0,0 +1,20 @@
+//DOMロード
+$(function(){
+    console.log('読み込んだよ');
+
+//#btnをクリックしたら
+    $('#btn').on('click',function(){
+
+        //ajaxでロード
+        $.ajax({
+            url:'omikuji.html',
+            type:'GET',
+            dataType:'html'
+        }).done(function(data){
+            console.log(data);
+        }).fail(function(){
+            console.log('ajax error');
+        });
+
+    });
+});
---
+++ js/omikuji.js
@@ -0,0 +1,32 @@
+
+var omikuji = [
+    {
+        result:'大吉',
+        love :'整う',
+        business:'成功',
+    },
+    {
+        result:'吉',
+        love :'耐える',
+        business:'動くな',
+    },
+    {
+        result:'凶',
+        love :'別れる',
+        business:'失敗',
+    }
+];
+
+console.log(omikuji);
+
+$(function(){
+    $('#btn').on('click',function(){
+
+    var randNum = Math.floor(Math.random()*omikuji.length);
+    console.log(randNum);
+    $('.resultKekka').text(omikuji[randNum].result);
+    $('.resultLove').text(omikuji[randNum].love);
+    $('.resultBusiness').text(omikuji[randNum].business);
+
+    });
+});
