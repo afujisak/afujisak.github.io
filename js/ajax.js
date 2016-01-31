b8e885620fc577d0b05283bf0835231d812a090d
afujisak <a.korira@gmail.com>
2016-01-31 16:41:29 +0900

    mydata

--- js/ajax.js
+++ js/ajax.js
@@ -7,21 +7,24 @@ $(function(){
 
         //ajaxでロード
         $.ajax({
-            url:'omikuji.html',
+            url:'mydata.json',
             type:'GET',
-            dataType:'html'
+            dataType:'json'
         }).done(function(data){
             //成功
             console.log(data);
+            // showContent(data);
+
         }).fail(function(){
             //失敗
             console.log('ajax error');
         });
 
     });
-    //表示させる関数
-    function showContent(data){
-        $('#container').append(data);
-    }
 
 });
+
+//表示させる関数
+function showContent(data){
+    $('#container').append(data);
+}
