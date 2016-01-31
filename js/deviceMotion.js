962ebe31050a0592d251b2f34fc2f18dba8848d6
afujisak <a.korira@gmail.com>
2016-01-31 14:51:59 +0900

    update

--- js/deviceMotion.js
+++ js/deviceMotion.js
@@ -9,8 +9,8 @@ $(function(){
         var z = evt.acceleration.z;
 
         $('#device li').eq(0).text('加速度x: ' + x);
-        $('#device li').eq(1).text('加速度x: ' + y);
-        $('#device li').eq(2).text('加速度x: ' + z);
+        $('#device li').eq(1).text('加速度y: ' + y);
+        $('#device li').eq(2).text('加速度z: ' + z);
     }
 
 });
