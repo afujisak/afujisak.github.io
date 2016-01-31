1ea2b494a8b6d69e12f44b5ae0ff27008ca68b9d
afujisak <a.korira@gmail.com>
2016-01-31 15:25:17 +0900

    up

--- js/touch.js
+++ js/touch.js
@@ -5,9 +5,9 @@ $(function(){
         $('#touchResult li').eq(0).text('touchstart したよ');
     });
     $('#ball').on('touchmove',function(){
-        $('#touchResult li').eq(1).text('touchstart したよ');
+        $('#touchResult li').eq(1).text('touchmove したよ');
     });
     $('#ball').on('touchend',function(){
-        $('#touchResult li').eq(2).text('touchstart したよ');
+        $('#touchResult li').eq(2).text('touchend したよ');
     });
 });
