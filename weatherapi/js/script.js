64982ad7bda98d9f7f684246b0e2231ba81a2dee
afujisak <a.korira@gmail.com>
2016-02-07 11:42:07 +0900

    weather update

--- weatherapi/js/script.js
+++ weatherapi/js/script.js
@@ -1,10 +1,30 @@
 $(function(){
-    //GPS
+    //GPS情報を取得するスクリプト
     navigator.geolocation.getCurrentPosition(function(pos){
-        var lat = pos.coords.latitude;
-        var lon = pos.coords.longitude;
+        var lat = pos.coords.latitude; //緯度
+        var lon = pos.coords.longitude; //経度
 
 
         console.log(lat, lon);
+
+        //APIキーは固定の値なので定数(const)で定義
+        const apikey = '3f92de7ccc9be29797ad35eaae9d4944'
+
+        //openweathermapのお天気情報のURL
+        var url = 'http://api.openweathermap.org/data/2.5/weather?lat=' + lat +'&lon=' + lon + '&appid=' + apikey;
+
+        $.ajax({
+            url;
+            dataType : 'json'
+        }).done(function(data){
+
+            console.log(data);
+
+        }).fail(function(){
+
+            console.log('ajax error!!');
+        });
+
     });
+
 });
