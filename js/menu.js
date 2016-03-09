cd3d777f7d200a356ecc95869220a122c52f3403
afujisak <a.korira@gmail.com>
2016-03-10 01:03:15 +0900

    up39

--- js/menu.js
+++ js/menu.js
@@ -2,20 +2,10 @@
 $(function(){
     console.log('DOM読み込んだよ');
 
-    //プルダウンメニュー
-    var flag = false;
-
-        $('#menu li').on('click',function(){
-            // console.log(this);
-        if(flag == false){
-            $('ul.submenu',this).slideDown();
-            flag = true;
-        }
-        else if(flag == true){
-            $('ul.submenu',this).slideUp();
-            flag = false;
-        }
-
+        $('#menu li').hover(function(){
+            $(this).children('ul').show();
+        },function(){
+            $(this).children('ul').hide();
         });
 
 });
